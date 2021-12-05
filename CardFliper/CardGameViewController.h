d347bd2f7799ac50b81a8bf8c2c44231c848e315
barak abadi <babadi@lightricks.com>
2021-12-05 10:50:46 +0200

    declare public abstract API, implement some functions of init state

--- CardFliper/CardGameViewController.h
+++ CardFliper/CardGameViewController.h
@@ -7,10 +7,25 @@
 
 #import <UIKit/UIKit.h>
 
+#import "Card.h"
+
 @class Deck;
 
 @interface CardGameViewController : UIViewController
 
 - (Deck *)createDeck; // abstract
+
+- (NSUInteger)getInitCardCount; //abstract
+
+- (NSInteger)getMatchMode; //abstract
+
+- (BOOL)addCardsAfterMatched; //abstract
+
+- (CGFloat)getCardRatio; //abstract
+
+- (UIView *)createViewOfCard:(Card *)card inFrame:(CGRect)frame; //abstract
+
+- (void)updateCardView:(UIView *)cardView usingCard:(Card *)card animation:(BOOL)toAnimate; //abstract
+
 @end
 
--- CardFliper/CardGameViewController.m
+++ CardFliper/CardGameViewController.m
@@ -8,95 +8,138 @@
 #import "CardGameViewController.h"
 
 #import "Deck.h"
+#import "PlayingCardDeck.h"
 #import "CardMatchingGame.h"
-#import "PlayingCardView.h"
+#import "Grid.h"
 
+#import "PlayingCard.h"
+#import "PlayingCardView.h"
 
 
 @interface CardGameViewController ()
 
 @property (strong, nonatomic) CardMatchingGame *game;
-@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+
+@property (strong, nonatomic) NSMutableArray *cardsViews; // alighn with game.cards
+
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+
 @property (weak, nonatomic) IBOutlet UIButton *resetButton;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *gameMode;
-@property (weak, nonatomic) IBOutlet UIView *CardDisplayAreaView;
 
-@property (weak, nonatomic) IBOutlet PlayingCardView *playingCardView;
+@property (weak, nonatomic) IBOutlet UIView *cardsPlaceHolderView;
 
+@property (strong, nonatomic) Grid *grid;
 
 @end
 
 @implementation CardGameViewController
 
-- (IBAction)tapOnCard:(UITapGestureRecognizer *)sender {
-  self.playingCardView.faceUp = !self.playingCardView.faceUp;
-}
+
 
 - (void)viewDidLoad{
-  [self updateUI:NO];
+  _cardsViews = [[NSMutableArray alloc] init];
+  _game = [[CardMatchingGame alloc] initWithCardCount:[self getInitCardCount] usingDeck:[self createDeck] inMatchMode:[self getMatchMode]];
+  
+  _grid = [[Grid alloc] init];
+  _grid.size = self.cardsPlaceHolderView.bounds.size;
+  _grid.minimumNumberOfCells = [self.game getNumberOfCardsInGame];
+  _grid.cellAspectRatio = [self getCardRatio];
+  
+  [self initCardsViewInPlaceHolder];
+  [self updateUI];
+  
 }
 
-- (CardMatchingGame *)game{
-    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:self.cardButtons.count usingDeck:[self createDeck] inMatchMode:[self getMatchMode]];
-    return _game;
+
+- (void)initCardsViewInPlaceHolder {
+  if (!_cardsViews) {
+    _cardsViews = [[NSMutableArray alloc] init];
+  }
+  
+  NSUInteger cardIndex = 0;
+  CGRect frame;
+  for (int row = 0; row < self.grid.rowCount; row++) {
+    for (int col = 0; col < self.grid.columnCount; col++) {
+      
+      Card *card = [self.game cardAtIndex:cardIndex];
+
+      frame = [self.grid frameOfCellAtRow:row inColumn:col];
+      
+      UIView *cardView = [self createViewOfCard:card inFrame:frame];
+      [self.cardsViews addObject:cardView];
+      
+      cardIndex++;
+    }
+  }
+  
 }
 
--(NSInteger) getMatchMode{
-//    return _gameMode.selectedSegmentIndex + 2;
-  return 2;
-}
 
--(Deck *)createDeck{
-    return nil;
-}
-
-- (IBAction)touchGameMode:(UISegmentedControl *)sender {
-    [self resetGame];
-}
-
-- (IBAction)touchCardButton:(UIButton *)sender {
-    NSUInteger chooseButtonIndex = [self.cardButtons indexOfObject:sender];
-    [self.game chooseCardAtIndex:chooseButtonIndex];
-    [self updateUI:NO];
-}
-
--(void) updateUI:(BOOL)isReset{
-  for(UIButton *cardButton in self.cardButtons){
-      NSUInteger cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
-      Card *card = [self.game cardAtIndex:cardButtonIndex];
-
-      [cardButton setTitle:[self textOfCard:card]
-                  forState:UIControlStateNormal];
-
-      [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
-
-      cardButton.enabled = !card.matched;
-
+-(void) updateUI {
+  for(int i = 0; i < self.cardsViews.count; i++){
+    Card *card = [self.game cardAtIndex:i];
+    UIView *cardView = self.cardsViews[i];
+    
+    [self updateCardView:cardView usingCard:card animation:YES];
+    [self.cardsPlaceHolderView addSubview:cardView];
+    
   }
   self.scoreLabel.text = [NSString stringWithFormat:@"Score: %ld",self.game.score];
-  self.gameMode.enabled = isReset;
-
-
 }
 
-
-- (NSString *)textOfCard:(Card *)card{
-    return [[NSString alloc] initWithString: card.chosen? card.contents:@""];
-}
-
--(UIImage *)backgroundImageForCard:(Card *)card{
-    return [UIImage imageNamed: card.chosen? @"cardfront" : @"cardback"];
-}
 
 - (void)resetGame {
     self.game = nil;
-    [self updateUI:YES];
+    [self updateUI];
 }
 
 - (IBAction)onClickResetButton:(UIButton *)sender {
     [self resetGame];
 }
+
+- (IBAction)tapOnCard:(UITapGestureRecognizer *)sender {
+  CGPoint tapLocation = [sender locationInView:self.cardsPlaceHolderView];
+  NSInteger index = -1;
+  for (int i = 0; i<self.cardsViews.count; i++) {
+    UIView *cardView = self.cardsViews[i];
+    if (CGRectContainsPoint(cardView.frame, tapLocation)) {
+      index = i;
+      break;
+    }
+  }
+  if (index > -1) {
+    [self.game chooseCardAtIndex:index];
+    [self updateUI];
+  }
+}
+
+
+- (Deck *)createDeck {
+  return nil;
+} // abstract
+
+- (NSUInteger)getInitCardCount {
+  return 0;
+} //abstract
+
+- (NSInteger)getMatchMode {
+  return 0;
+} //abstract
+
+- (BOOL)addCardsAfterMatched {
+  return nil;
+} //abstract
+
+- (CGFloat)getCardRatio {
+  return 0;
+} //abstract
+
+- (UIView *)createViewOfCard:(Card *)card inFrame:(CGRect)frame {
+  return nil;
+} //abstract
+
+- (void)updateCardView:(UIView *)cardView usingCard:(Card *)card animation:(BOOL)toAnimate {
+} //abstract
 
 
 @end
